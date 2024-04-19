1d99bb481213ea56c1aa914575be0f32b9489cca
João Pedro Oliveira <122324367+pasteldiventu@users.noreply.github.com>
2024-04-18 20:52:17 -0400

    Add files via upload

---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Titular struct {
+	Nome      string
+	CPF       string
+	Profissao string
+}
+
+type ContaCorrente struct {
+	Titular                    Titular
+	NumeroAgencia, NumeroConta int
+	saldo                      float64
+}
+
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "saldo insuficiente"
+	}
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Deposito realizado com sucesso", c.saldo
+	} else {
+		return "Valor do deposito menor que zero", c.saldo
+	}
+}
+
+func (c *ContaCorrente) Tranferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
+	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
+		c.saldo -= valorDaTransferencia
+		contaDestino.Depositar(valorDaTransferencia)
+		return true
+	} else {
+		return false
+	}
+}
+
+func (c *ContaCorrente) ObterSaldo() float64 {
+	return c.saldo
+}
+
+type ContaPoupanca struct {
+	Titular                              Titular
+	NumeroAgencia, NumeroConta, Operacao int
+	saldo                                float64
+}
+
+func (c *ContaPoupanca) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "saldo insuficiente"
+	}
+}
+
+func (c *ContaPoupanca) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Deposito realizado com sucesso", c.saldo
+	} else {
+		return "Valor do deposito menor que zero", c.saldo
+	}
+}
+
+func (c *ContaPoupanca) ObterSaldo() float64 {
+	return c.saldo
+}
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
+func main() {
+	contaDoDenis := ContaPoupanca{}
+	contaDoDenis.Depositar(100)
+	PagarBoleto(&contaDoDenis, 60)
+
+	fmt.Println(contaDoDenis.ObterSaldo())
+
+	contaDaLuisa := ContaCorrente{}
+	contaDaLuisa.Depositar(500)
+	PagarBoleto(&contaDaLuisa, 1000)
+
+	fmt.Println(contaDaLuisa.ObterSaldo())
+
+}
